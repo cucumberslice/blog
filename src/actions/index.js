3397a4810111ccc2010faeb6236881c2f69b57f1
cucmberslice <ywblackberry@gmail.com>
2019-10-10 00:31:17 -0400

    hooked redux-thunk in index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,9 @@
-export const fetchPosts = () => {
+import jsonPlaceHolder from '../apis/jsonPlaceholder'
+
+export const fetchPosts = async () => {
+    const response = await jsonPlaceHolder.get('/posts')
     return {
-        type: 'FETCH_POSTS'
+        type: 'FETCH_POSTS',
+        payload: response
     }
 }
