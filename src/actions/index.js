0c62848472d2378d5b03251c150e098b03fae684
cucmberslice <ywblackberry@gmail.com>
2019-10-18 11:07:24 -0400

    refactored fetchUser back to old function where it fetches all the users

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,18 +7,32 @@ export const fetchPosts = () => async dispatch => {
         dispatch({ type: 'FETCH_POSTS', payload: response.data })
     }
 
-    export const fetchUser = (id) =>  dispatch => {
-        _fetchUser(id, dispatch)
-    }
+    // export const fetchUser = (id) =>  dispatch => {
+    //     _fetchUser(id, dispatch)
+    // }
 
     // this use of lodash with memoize solves our over fetching of users issues
     /*_.memoize runs the function only one time and if its called again, 
     it doesnt rerun the function but instead returns it from its previous memory*/
     
-    const _fetchUser = _.memoize(async(id,dispatch) => {
+    // const _fetchUser = _.memoize(async(id,dispatch) => {
+    //     const response = await jsonPlaceHolder.get(`/users/${id}`)
+
+    //     dispatch({ type: 'FETCH_USER', payload: response.data })
+    // })
+
+    export const fetchUser = id => async dispatch => {
         const response = await jsonPlaceHolder.get(`/users/${id}`)
 
         dispatch({ type: 'FETCH_USER', payload: response.data })
-    })
+    }
 
+    /**
+     * fethchPostsAndUsers()
+     * call 'fetchPosts;
+     * Get List of posts
+     * find all unique userId's from list of posts
+     * Iterate over unique userId's
+     * call 'fetchUser' with each userId
+     */
   
